ffed033fd103b727213f78c88aa7fef4c755cbd2
Franzie Yoogan <franzieyoogan@gmail.com>
2024-06-02 16:21:25 -0300

    errorLoginAlert

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -34,7 +34,33 @@
 
 <div style="position: absolute; bottom:0; width:100%">
 
+<section class="containerErrorAll">
+
+
+@if(isset($error))
+
+<div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+  <span class="font-medium">ERROR!</span> Invalid credentials.
+</div>
+
+<script>
+
+  setTimeout(() => {
+
+    window.location.href = "http://127.0.0.1:8000/login"
+
+  }, 2000);
+
+</script>
+
+</section>
+
+@endif
+<div style="width: 100%;">
+
 @include('footer')
+
+</div>
     
 </div>
 
